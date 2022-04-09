ecb97c1934ca2cc5951a3117cbe49e6a4202f589
Beables <95437279+BeaSiM@users.noreply.github.com>
2022-04-09 09:16:21 -0300

    Comando For

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+
+	for($i = 0; $i<100; $i++) {
+    	echo "$i Flamengo, Campeão de Tudo em 2022<br>\n";
+    }
+
+/* Para quebra do código fonte inserir "\n" */
+ ?>
+</body>
+</html>
