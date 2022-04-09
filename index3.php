f015f7619d03611fac9be0cb6a558c51deb4b056
Beables <95437279+BeaSiM@users.noreply.github.com>
2022-04-09 09:45:11 -0300

    Comando Do While

---
+++ index3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto Sábado - Do While</title>
+</head>
+<body>
+	<?php
+		$i=0;
+		do {echo "$i Flamengo, Campeão de Tudo em 2022<br>\n";
+			$i++;
+		}
+		while ($i <= 100);
+	?>
+
+</body>
+</html>
