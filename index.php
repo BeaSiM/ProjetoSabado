48b961e2dd54ea15661d85d2e8055d236cc5045a
Beables <95437279+BeaSiM@users.noreply.github.com>
2022-05-27 22:28:26 -0300

    Projeto
    Projeto Telecall

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-<?php 
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--Import Google Icon Font-->
+      	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      	<!--Import materialize.css-->
+      	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-	for($i = 0; $i<100; $i++) {
-    	echo "$i Flamengo, Campeão de Tudo em 2022<br>\n";
-    }
-
-/* Para quebra do código fonte inserir "\n" */
- ?>
-</body>
+      	<!--Let browser know website is optimized for mobile-->
+      	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>Login</title>
+	</head>
+	<body>
+		<!--JavaScript at end of body for optimized loading-->
+      	<script type="text/javascript" src="js/materialize.min.js"></script>
+		<nav>
+    		<div class="nav-wrapper">
+		    	<a href="#" class="brand-logo"><img src="img/logo.jpeg" class="responsive" width="150" height="64"></a>
+		    	<ul id="nav-mobile" class="right hide-on-med-and-down">
+			    	<li><a href="sass.html">Sass</a></li>
+			        <li><a href="badges.html">Components</a></li>
+			        <li><a href="collapsible.html"><img src="img/bea.jpeg" id="img" style="width:50px"></a></li>
+			        <li>
+			        	<!-- Switch -->
+						<div class="switch">
+						    <label>
+						      Off
+						      <input type="checkbox">
+						      <span class="lever"></span>
+						      On
+						    </label>
+						</div>
+					</li>
+		    	</ul>
+    		</div>
+  		</nav>
+	</body>
 </html>
